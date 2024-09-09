c7279fbd7d13bd2918f59c985a95c712190d40a7
MW Developer Team <101448833+MWDeveloperTeam@users.noreply.github.com>
2024-09-09 15:48:34 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+import { useRoutes } from "react-router-dom";
+import Nav from "./components/Nav";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Contact from "./pages/contact/Contact";
+import Layout from "./components/Layout";
 
 const App = () => {
-  return (
-    <div>
-      <h1>Hello This is Home Page</h1>
-    </div>
-  )
-}
+  const elements = useRoutes([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "about",
+          element: <About />,
+        },
+        {
+          path: "contact",
+          element: <Contact />,
+        },
+      ],
+    },
+  ]);
+  return elements;
+};
 
-export default App
+export default App;
