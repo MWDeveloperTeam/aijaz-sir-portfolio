b6aa1e69b094927509b2eba6ecfdb5133b9557d8
MW Developer Team <101448833+MWDeveloperTeam@users.noreply.github.com>
2024-09-12 23:33:16 +0530

    work page completed

--- src/components/Graphic Design/GraphicDesign.jsx
+++ src/components/Graphic Design/GraphicDesign.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import { Color } from "../../assets/Colors.js";
 import Photos from "./Photos.js";
 import { ColumnsPhotoAlbum } from "react-photo-album";
 import "react-photo-album/columns.css";
@@ -11,17 +12,21 @@ import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
 import "yet-another-react-lightbox/plugins/thumbnails.css";
 import "./style.css";
 
-const GraphicDesign = ({ photo }) => {
+const GraphicDesign = ({Photoss}) => {
   const [index, setIndex] = useState(-1);
-  console.log(photo);
+  const [newPhoto, SetNewPhoto] = useState(Photoss.slice(0, 10));
+  const photoHander = () => {
+    SetNewPhoto(Photoss.slice(0, Photoss.length));
+  };
 
   return (
     <GraphicSection>
       <ColumnsPhotoAlbum
-        photos={Photos}
+        photos={newPhoto}
         targetRowHeight={500}
         onClick={({ index }) => setIndex(index)}
         spacing={20}
+        lazy={true}
         columns={(containerWidth) => {
           if (containerWidth < 400) return 1;
           if (containerWidth < 800) return 2;
@@ -30,12 +35,15 @@ const GraphicDesign = ({ photo }) => {
         }}
       />
       <Lightbox
-        slides={Photos}
+        slides={newPhoto}
         open={index >= 0}
         index={index}
         close={() => setIndex(-1)}
         plugins={[Fullscreen, Thumbnails]}
       />
+      <div className="see_more_wrapper">
+        <button onClick={photoHander}>See More</button>
+      </div>
     </GraphicSection>
   );
 };
@@ -47,4 +55,26 @@ const GraphicSection = styled.div`
   width: 100%;
   height: auto;
   /* background-color: maroon; */
+
+  .see_more_wrapper {
+    margin-top: 6rem;
+    text-align: center;
+
+    button {
+      padding: 1.6rem 6rem;
+      background-color: ${Color.seconderyColor};
+      border-radius: 5rem;
+      cursor: pointer;
+      transition: ease-in-out 0.3s;
+      font-size: 2rem;
+      border: none;
+
+
+      &:hover {
+        border: 0.1rem solid ${Color.seconderyColor};
+        background-color: ${Color.primaryColor};
+        color: ${Color.seconderyColor};
+      }
+    }
+  }
 `;
