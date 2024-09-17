0588025c56a69a901a5b77c51b35851edb68ae0a
MW Developer Team <101448833+MWDeveloperTeam@users.noreply.github.com>
2024-09-17 13:17:14 +0530

    404 added

--- src/pages/404/404.jsx
+++ src/pages/404/404.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+import NotFoundImg from "../../assets/imgs/404.webp";
 
 const PageNotFound = () => {
   return (
-    <div>
-      <h1>404 Page Not Found</h1>
-    </div>
-  )
-}
+    <Section>
+      <img src={NotFoundImg} alt="Page Not Found" />
+    </Section>
+  );
+};
 
-export default PageNotFound
+export default PageNotFound;
 const Section = styled.div`
+  width: 100%;
+  height: auto;
+  background-color: #000000;
+
+  img {
     width: 100%;
-    height: 100vh;
-    background-color: red;
-`
+    height: auto;
+  }
+`;
